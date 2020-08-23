c339eaa77306ed2787c88121860f9f0420aa3b8e
RomBik17 <r.rafalyuk@gmail.com>
2020-08-23 21:31:23 +0300

    new pattern

---
+++ sequential_patterns/double_buffering.cpp
@@ -0,0 +1,63 @@
+
+#include <iostream>
+
+//приблиэительная графическая подсистема вывода пикселей в бефер обмена
+class Framebuffer
+{
+public:
+    Framebuffer() { clear(); }
+
+    void clear() {
+        for (int i = 0; i < WIDTH * HEIGHT; i++) {
+            pixels_[i] = WHITE;
+        }
+    }
+
+    void draw(int x, int y) {
+        pixels_[(WIDTH * y) + x] = BLACK;
+    }
+
+    const char* getPixels() {
+        return pixels_;
+    }
+
+private:
+    static const int WIDTH = 160;
+    static const int HEIGHT = 120;
+
+    char pixels_[WIDTH * HEIGHT];
+};
+
+class Scene
+{
+public:
+    Scene()
+        : current_(&buffers_[0]),
+        next_(&buffers_[1])
+    {}
+
+    //отрисовываем следующий кадр, и меняем буфера местами
+    void draw() {
+        next_->clear();
+
+        next_->draw(1, 1);
+        // ...
+        next_->draw(4, 3);
+
+        swap();
+    }
+
+    Framebuffer& getBuffer() { return *current_; }
+
+private:
+    void swap() {
+        // Just switch the pointers.
+        Framebuffer* temp = current_;
+        current_ = next_;
+        next_ = temp;
+    }
+
+    Framebuffer  buffers_[2];
+    Framebuffer* current_;
+    Framebuffer* next_;
+};
